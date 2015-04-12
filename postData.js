25e8ac2e4defba9278ac7e49ae553a66c0566867
ilnuirbat <ilnuribat@gmail.com>
2015-04-12 15:46:20 +0500

    one more quick commit

--- postData.js
+++ postData.js
@@ -2,11 +2,12 @@ var Var = require('./variables.js');
 var sql = require('./sql.js');
 
 Var.app.post('/otmetka', function(request, response) {
-    var body = request.body;
-    console.log(body);
-	var name = body['name'];
-	var phone = body['phone'];
-	var human = body['human'];
-	
+	var query = Var.url.parse(request.url).query;
+	var params = Var.queryString.parse(query);
+    
+	var typeOfMarking = params['type'];
+	var addingRows = params['data'];
+	var day = new Date.getTime() / (1000 * 60 * 60 * 24);
+	var queryString = "INSERT INTO control(id_student, day)";
 	response.send("otmetka");
 });
