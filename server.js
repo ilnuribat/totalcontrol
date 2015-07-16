0969b85f25c05a84476f57808760859c11cf1f57
ilnuribat <ilnuribat@gmail.com>
2015-07-16 14:44:39 +0500

    require auth.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var Var = require('./variables.js');
 require('./postData.js');
 require('./getData.js');
 require('./init.js');
+require('./auth.js');
 
 Var.app.listen(80);
 console.log("Server стартанул");
